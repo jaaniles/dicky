6f1a36d85f151c525733c71ab5f3d19b5773f111
Jaaniles <jaani@kooders.fi>
2016-12-29 21:34:46 +0200

    Task expires, handling situations where client not online etc

--- public/js/utils.js
+++ public/js/utils.js
@@ -4,3 +4,67 @@ function removeClass(element, className){
 function addClass(element, className){
     document.getElementById(element).classList.add(className)
 }
+function getUser(){
+    var user = firebase.auth().currentUser
+    if (!user){
+        return false
+    }
+    return user
+}
+function signOut(){
+    firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+        $("#sessions_list").html()
+        $("#sessions").addClass("hide")
+        $("#signOutBtn").addClass("hide")
+        $("#firebaseui-auth-container").removeClass("hide")
+    })
+}
+
+function home(){
+    window.location.href = "/"
+}
+function showAlert(text){
+    $("#footerAlert").removeClass("hide")
+    $("#alertText").html(text)
+}
+function hideAlert(){
+    $("#footerAlert").addClass("hide")
+}
+function openActions() {
+    $("#actions").css("width", "100%")
+}
+function closeActions() {
+    $("#actions").css("width", "0%")
+}
+function openTask(){
+    $("#task_overlay").css("width", "100%")
+}
+function closeTask(){
+    $("#task_overlay").css("width", "0%")
+}
+
+    /*--------------------------------------------*/
+    /*
+    var socket = io.connect(window.location.origin)
+    document.addEventListener("DOMContentLoaded", function(event) { 
+        //join(window.location.pathname.substring(1))
+    });
+    function add_player_to_session(user, sessionId){
+        console.log(user.uid)
+        database.ref("players/"+user.uid).set({
+        }).then(function(){
+            console.log("Joined session")
+        })
+    }
+    socket.on("join_success", function(data){
+        window.history.pushState("", "", data.sessionId);
+    })
+    function createAlert(text){
+        var alert = '<div class="alert">'
+        +text
+        +'</div>'
+        return alert
+    }
+    */
+    /*--------------------------------------------*/
